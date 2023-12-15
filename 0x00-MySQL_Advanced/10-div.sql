010f82c0ac442a9be8caf5b1a65d8427ae58a082
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-12-14 16:11:42 -0800

    Task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -7,10 +7,11 @@
 -- a, INT
 -- b, INT
 -- And returns a / b or 0 if b == 0
-DELIMITER $$ 
+DELIMITER // 
 DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
 	RETURN (IF (b = 0, 0, a / b));
-END $$
+END //
 DELIMITER ;
