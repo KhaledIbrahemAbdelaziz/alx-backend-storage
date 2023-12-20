0ca22d0be8810639bc7ab73e7ef3190705d9a371
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-12-20 11:32:35 -0800

    Task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,7 @@ import uuid
 class Cache:
     """Implements the cache class"""
     def __init__(self):
-        """Initializes the attributes of the Redis client."""
+        """stores an instance of the Redis client."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
@@ -17,6 +17,22 @@ class Cache:
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Takes and stores a data argument and returns a string."""
-        keys = str(uuid.uuid4())
-        self._redis.mset({keys: data})
-        return keys
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self,
+            key: str, fn: Optional[Callable] = None) -> str:
+        """Takes a key string argument and an optional.
+        Callable argument named fn. This callable will be used to\
+            convertthe data back to a desired format."""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, data: str) -> str:
+        """Returns str value of decoded byte """
+        return data.decode('utf-8', 'strict')
+
+    def get_int(self, data: str) -> int:
+        """Returns int value of decoded byte """
+        return int(data)
