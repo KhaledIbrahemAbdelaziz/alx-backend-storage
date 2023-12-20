a746d4209f64360c0605fb6bce210dd384ce5aae
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-12-20 11:37:52 -0800

    All Tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,45 @@ import redis
 import uuid
 
 
+def call_history(method: Callable) -> Callable:
+    """Stores the history of inputs and outputs for a particular function"""
+    method_keys = method.__qualname__
+    inputs, outputs = method_keys + ':inputs', method_keys + ':outputs'
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(inputs, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(result))
+        return result
+    return wrapper
+
+
+def count_calls(method: Callable) -> Callable:
+    """Creates and returns function that increments the count \
+        for that key every time the method is called and returns \
+        the value returned by the original method"""
+    method_keys = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(method_key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """Displays the history of calls of a particular function"""
+    method_keys = method.__qualname__
+    inputs, outputs = method_keys + ':inputs', method_keys + ':outputs'
+    redis = method.__self__._redis
+    method_counts = redis.get(method_keys).decode('utf-8')
+    print(f'{method_keys} was called {method_counts} times:')
+    IOTuple = zip(redis.lrange(inputs, 0, -1), redis.lrange(outputs, 0, -1))
+    for inp, outp in list(IOTuple):
+        attr, data = inp.decode("utf-8"), outp.decode("utf-8")
+        print(f'{method_keys}(*{attr}) -> {data}')
+        
 class Cache:
     """Implements the cache class"""
     def __init__(self):
